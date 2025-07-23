e980f9cfadaece9e35f346e6dc98947f5d6775a2
SE <155702977+bulletproofvest@users.noreply.github.com>
2025-07-23 15:36:43 +0900

    6

---
+++ src/ch01_variable_operator/ShowJumsu.java
@@ -0,0 +1,13 @@
+package ch01_variable_operator;
+
+public class ShowJumsu {
+    public static void main(String[] args) {
+        String name = "홍길동";
+        double a = 40.0;
+        double b = 43.0;
+        double c = (a+b) / 2;
+
+        System.out.println("이름 : " + name + "\n국어 : " + a + "\n영어 : " + b + "\n평균 : " + c);
+
+    }
+}
