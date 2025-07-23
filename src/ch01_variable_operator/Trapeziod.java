deee5b4116766ac4f0e9c15e33e5bb9178e65bc2
SE <155702977+bulletproofvest@users.noreply.github.com>
2025-07-23 15:57:04 +0900

    6

---
+++ src/ch01_variable_operator/Trapeziod.java
@@ -0,0 +1,17 @@
+package ch01_variable_operator;
+
+public class Trapeziod {
+    public static void main(String[] args) {
+        double bot = 20;
+        double top = 10;
+        double hei = 15;
+        double area = (bot+top)*hei/2;
+
+        System.out.println("밑변 : " + bot);
+        System.out.println("윗변 : " + top);
+        System.out.println("높이 : " + hei);
+        System.out.println("넓이 : " + area);
+
+
+    }
+}
