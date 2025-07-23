d9347bcb22de9d76f9c9d2f64a2f5273b4f9c602
SE <155702977+bulletproofvest@users.noreply.github.com>
2025-07-23 12:05:21 +0900

    5

--- src/ch01_variable_operator/MyPrint.java
+++ src/ch01_variable_operator/MyPrint.java
@@ -1,14 +1,23 @@
 package ch01_variable_operator;
+/*
+    작성자 : 홍길동
+    작성일 : 7울 23일
+    목적 : 처음 해보는 자바 프로그램
+ */
 
 public class MyPrint {
     public static void main(String[] args) {
+        // 한줄 짜리 주석 comment
         System.out.println("홍");
         System.out.println();
         System.out.println("길");
         System.out.println("동");
         System.out.print("대한 민국");
-        System.out.println(" 화이팅"); //메소드 : 어떠한 기능을 함
+        System.out.println(" 화이팅"); //메소드
         System.out.println("\"예체능\" 프로그램 메인 MC \'강호동\'"); //sout
+        // ctrl+/ : 해당 줄 주석 처리 + 마우스 드래그로 여러줄 동시 사용 가능
+        // shift+Tab <> Tab
+        // 줄정리 Ctrl + Alt + L
 
         System.out.println("System.out.println(\"이름\t국어\t영어\t수학\");"); //세미콜론 출력 가능
 
