e82822441fe4cbf9468876a030933e92efe73750
SE <155702977+bulletproofvest@users.noreply.github.com>
2025-07-23 18:13:55 +0900

    6

--- src/ch01_variable_operator/Myclasss.java
+++ src/ch01_variable_operator/Myclasss.java
@@ -1,5 +1,6 @@
 package ch01_variable_operator;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,18 +9,32 @@ import java.util.Scanner;
 public class Myclasss {
     public static void main(String[] args) {
 
-        List<String> listmak = new ArrayList<>();
-
-//        List<String> queList = new ArrayList<>("좋아하는 음식", "좋아하는 색상", "좋아하는 음악","좋아하는 영화", "좋아하는 책", "첫사랑");
+        ArrayList<String> MyList = new ArrayList<>();
+        ArrayList<String> YouList = new ArrayList<>();
+        ArrayList<String> queList = new ArrayList<>(Arrays.asList("좋아하는 음식", "좋아하는 색상", "좋아하는 음악","좋아하는 영화", "좋아하는 책", "첫사랑"));
         // ~~의 ~~은?
 
+        int quenum = queList.size();
         Scanner sc = new Scanner(System.in);
+        String Myname;
+        String Youname;
 
         System.out.println("유저 이름:");
+        Myname = sc.nextLine();
+
+        for(int i = 0; i<quenum; i++){
+            System.out.println(Myname + "의" + queList.get(i) + "은?");
+            String input = sc.nextLine();
+            MyList.add(input);
+        }
+
+        System.out.println(MyList);
 
         System.out.println("상대 유저 이름:");
 
+        for(int i = 0; i<quenum; i++){
 
+        }
 
     }
 }
