1441e4f84a3cc356a009ac37d826a997712e7334
SE <155702977+bulletproofvest@users.noreply.github.com>
2025-07-23 11:18:44 +0900

    5

--- src/ch01_variable_operator/MyPrint.java
+++ src/ch01_variable_operator/MyPrint.java
@@ -2,10 +2,16 @@ package ch01_variable_operator;
 
 public class MyPrint {
     public static void main(String[] args) {
-       System.out.println("홍");
-       System.out.println();
-       System.out.println("길");
-       System.out.println("동");
+        System.out.println("홍");
+        System.out.println();
+        System.out.println("길");
+        System.out.println("동");
+        System.out.print("대한 민국");
+        System.out.println(" 화이팅"); //메소드 : 어떠한 기능을 함
+        System.out.println("\"예체능\" 프로그램 메인 MC \'강호동\'"); //sout
+
+        System.out.println("System.out.println(\"이름\t국어\t영어\t수학\");"); //세미콜론 출력 가능
+
     }
 
 }
