8d4a3bcad05705a54923b3b28938f9f326864633
SE <155702977+bulletproofvest@users.noreply.github.com>
2025-07-23 11:11:01 +0900

    3

--- src/ch01_variable_operator/MyPrint.java
+++ src/ch01_variable_operator/MyPrint.java
@@ -3,7 +3,9 @@ package ch01_variable_operator;
 public class MyPrint {
     public static void main(String[] args) {
        System.out.println("홍");
+       System.out.println();
        System.out.println("길");
+       System.out.println("동");
     }
 
 }
