004a5138c523f6378aec7baeed74280b9e7229c5
SE <155702977+bulletproofvest@users.noreply.github.com>
2025-08-01 09:55:25 +0900

    6

---
+++ src/ch04_class/Saram04.java
@@ -0,0 +1,22 @@
+package ch04_class;
+
+public class Saram04 {
+    // 기본생성자 : 초기화
+    private String name;
+    private double height;
+    private double weight;
+    private String hobby;
+    private String blood;
+
+    // 생성자
+    // class 이름과 동일, 반환타입 명시 x
+
+    public Saram04(){
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.hobby = hobby;
+        this.blood = blood;
+    }
+
+}
---
+++ src/ch04_class/SaramMain04.java
@@ -0,0 +1,9 @@
+package ch04_class;
+
+public class SaramMain04 {
+    public static void main(String[] args) {
+        Saram04 soo = new Saram04();
+
+        
+    }
+}
